e13610d41d2eda4008a327a4cc7ebe7ab0a9c367
unknown <dulon587@gmail.com>
2021-12-31 23:12:12 +0600

    print

--- resources/views/admin/pages/services/services_details.blade.php
+++ resources/views/admin/pages/services/services_details.blade.php
@@ -1,5 +1,7 @@
 @extends("admin.index")
 @section('content')
+
+<div id="divToPrint">
 <h1>Service Details</h1>
 
 <p>
@@ -10,9 +12,24 @@
 <p>Services Details: {{$services->description}}</p>
 <p>Services Category: {{$services->category}}</p>
 
+</div>
+
 <a class="btn btn-danger" type="button" href="{{route('admin.services')}}">Back</a>
+<input class="btn btn-primary" type="button" onClick="Download('divToPrint');" value="Download">
 
 <!-- <lable>Services Name:</lable>
 <input type="text" class="form-control" value="{{$services->name}}">
 <input type="file" class="form-control"> -->
 @endsection
+
+<script>
+    function Download(divName)
+    {
+        var downloadContents = document.getElementById(divName).innerHTML;
+        var originalContents = document.body.innerHTML;
+        document.body.innerHTML = downloadContents;
+        window.print();
+        document.body.innerHTML = originalContents;
+
+    }
+</script>
