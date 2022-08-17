48cf308a55b363063b9ff03093626f4b78a63b76
Dulon18 <dulon587@gmail.com>
2022-08-17 23:43:39 +0600

    provider info

--- resources/views/admin/pages/serviceProvider/SP-list.blade.php
+++ resources/views/admin/pages/serviceProvider/SP-list.blade.php
@@ -4,7 +4,7 @@
 <br>
 
 <h1>Service Provider List</h1><br>
-<!-- <a href="{{route('admin.serviceProvider.add')}}" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#myModal">Add</a> -->
+<a href="{{route('admin.serviceProvider.add')}}" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#myModal">Add</a>
              
 <br>
 <br>
@@ -13,35 +13,34 @@
     <tr>
       <th scope="col">ID</th>
       <th scope="col">Name</th>
-     
       <th scope="col">Email</th>
       <th scope="col">PhoneNumber</th>
-      <!-- <th scope="col">Status</th>
-      <th scope="col">Action</th> -->
+      <th scope="col">Status</th>
+      <th scope="col">Action</th>
     </tr>
   </thead>
   <tbody>
-    @foreach($providersDetails as $key=>$p)
+    @foreach($providers as $key=>$p)
 
      <tr>
-         <th>{{$key+1}}</th>
+         <td>{{$key+1}}</td>
          <td>{{$p->name}}</td>
        
          <td>{{$p->email}}</td>
-         <td>{{$p->phone}}</td>
-         <!-- <td>
+         <td>{{$p->phn}}</td>
+         <td>
            @if($p->status==1)
            <a type="button" href="{{route('admin.sprovider.status',$p->id)}}" onclick="return confirm('Are you sure??')" class='btn btn-success'>Active</a>
            @else
            <a type="button" href="{{route('admin.sprovider.status',$p->id)}}" class='btn btn-danger'>Inactive</a>
            @endif
-         </td> -->
-         <!-- <td>
+         </td>
+         <td>
            
            <a href="{{route('admin.sprovider.view',$p->id)}}" class='btn btn-primary'><i class="uil uil-eye"></i></a>
            <a href="{{route('admin.sprovider.edit',$p->id)}}" class='btn btn-info'><i class="uil uil-edit"></i></a>
            <a href="{{route('admin.sprovider.delete',$p->id)}}"class='btn btn-danger' onclick="return confirm('Are you sure to delete??')"><i class="uil uil-trash-alt"></i></a>
-         </td> -->
+         </td>
          
       </tr>
 
